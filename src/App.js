cafb16f57e22671ea862f76816df87d8a4caa942
shahinbevy <meherullah.shahin@bevycommerce.com>
2024-10-06 15:50:00 +0600

    update code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         <h1> Hello World </h1>
-      <h2> My name is Abdelali Jadelmoula </h2>
+      <h2> My name is Shahin </h2>
 <h3> I am in love with devops (CI/CD) pipeline </h3>
         </p>
         <a
